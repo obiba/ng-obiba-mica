34dd22adee152926801236a957afe8ec5cdba5d8
Ramin Haeri Azad <rhaeri@maelstrom-research.org>
2017-01-30 10:45:04 -0500

    Updated ng-obiba version.

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -3,7 +3,7 @@
  * https://github.com/obiba/ng-obiba-mica
 
  * License: GNU Public License version 3
- * Date: 2017-01-27
+ * Date: 2017-01-30
  */
 /*
  * Copyright (c) 2017 OBiBa. All rights reserved.
