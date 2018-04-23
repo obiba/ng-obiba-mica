72c5f94766b7a40b8837dd338795192e9343f15e
Yannick Marcon <yannick.marcon@obiba.org>
2018-04-23 14:04:38 +0200

    ng-obiba 1.7.0

--- dist/ng-obiba-mica.js
+++ dist/ng-obiba-mica.js
@@ -3,7 +3,7 @@
  * https://github.com/obiba/ng-obiba-mica
  *
  * License: GNU Public License version 3
- * Date: 2018-04-19
+ * Date: 2018-04-23
  */
 /*
  * Copyright (c) 2018 OBiBa. All rights reserved.
